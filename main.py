1124e3a7bda86b5271bacc7248fb14bc8167499b
prashanth shanmuganathan <sprastar@MacBook-Pro-7.cable.rcn.com>
2018-08-19 12:02:49 -0400

    Reshaped the site

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import url_for
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return render_template ("index.html")
-
-@app.route("/about")
 def about():
-    return render_template ("about.html")
-
-@app.route("/resume")
-def resume():
-    return render_template ("resume.html")
+    return render_template('index.html') 
 
 
-@app.route("/juypter")
-def juypter():
-    return render_template ("juypter.html")
-
-@app.route("/projects")
-def projects():
-    return render_template ("projects.html")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
